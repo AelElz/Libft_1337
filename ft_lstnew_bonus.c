8171d73746c27f79f8dad7e3392fad8d448b428d
ael-azha <dbsperfect2004@gmail.com>
2024-11-04 19:48:06 +0100

    Libft

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-azha <ael-azha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:06:29 by ael-azha          #+#    #+#             */
-/*   Updated: 2024/11/04 16:30:40 by ael-azha         ###   ########.fr       */
+/*   Updated: 2024/11/04 19:44:37 by ael-azha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	
+	t_list	*new_nodes;
+
+	new_nodes = (t_list *)malloc(sizeof(t_list));
+	if (!new_nodes)
+		return (NULL);
+	new_nodes -> content = content;
+	new_nodes -> next = NULL;
+	return (new_nodes);
 }
