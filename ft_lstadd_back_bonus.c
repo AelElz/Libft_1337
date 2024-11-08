2ce83de3ac93928588495741fa245fc2c9c68a87
ael-azha <dbsperfect2004@gmail.com>
2024-11-08 22:51:11 +0100

    Libft

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ael-azha <ael-azha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 23:00:26 by ael-azha          #+#    #+#             */
-/*   Updated: 2024/11/08 15:19:59 by ael-azha         ###   ########.fr       */
+/*   Updated: 2024/11/08 22:36:22 by ael-azha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (!lst)
+	if (!lst || !new)
 		return ;
 	if (*lst == NULL)
 		*lst = new;
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ael-azha <ael-azha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:37:44 by ael-azha          #+#    #+#             */
-/*   Updated: 2024/11/08 16:46:11 by ael-azha         ###   ########.fr       */
+/*   Updated: 2024/11/08 22:49:44 by ael-azha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 	size_t	dst_len;
 	size_t	src_len;
-	size_t	total_len;
 
 	dst_len = 0;
 	src_len = 0;
+	if (dst == NULL || src == NULL)
+		return (0);
 	while (dst[dst_len])
 		dst_len++;
 	while (src[src_len])
 		src_len++;
-	total_len = dst_len + src_len;
 	if (dstsize <= dst_len)
 		return (dstsize + src_len);
 	i = 0;
@@ -35,5 +35,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	return (total_len);
+	return (dst_len + src_len);
 }
